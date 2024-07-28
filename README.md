aa47120c45e24516c39228d0f390234d17cf8edd
Alex Shore <alex@shore.me.uk>
2024-07-29 00:00:31 +0100

    added rust functionality

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 # Brainfuck Python Transpiler
 
 Repo contains brainfuck transpilers written in python for the following languages:
+
 * Python
-* C
-* C++
 * Rust
 
 ## What the code in this repo _will_ do:
@@ -14,4 +13,5 @@ Repo contains brainfuck transpilers written in python for the following language
 ## What the code in this repo _won't_ do:
 
 * Actually make the code runable.
-> If you choose C, C++, Rust, or any language I add in the future that requires compiling, it will not do the compiling for you, it will simply spit out the source code into the target folder and you can compile it from there either with gcc, rustc, or any other relevant compiler.
+
+> If you choose Rust, or any language I add in the future that requires compiling, it will not do the compiling for you, it will simply spit out the source code into the target folder and you can compile it from there either with gcc, rustc, or any other relevant compiler.
--- src/generator.py
+++ src/generator.py
@@ -50,12 +50,40 @@ class RustGenerator(Generator):
         tkns.Pointer_Right: "pointer += {}\n",
         tkns.Pointer_Left: "pointer -= {}\n",
         tkns.Value_Increase: "memory[pointer] += {}\n",
+        tkns.Value_Decrease: "memory[pointer] -= {}\n",
+        tkns.While_Start: "while memory[pointer] > 0 {\n",
+        tkns.While_End: "}\n",
+        tkns.Char_Get: "{let mut var = String::new(); std::io::stdin().read_line(&mut var); memory[pointer] = var.chars().nth(0).unwrap() as u8}\n",
+        tkns.Char_Put: 'println!("{}", memory[pointer] as char)\n'
     }
+
+    def generate(self, tokens) -> str:
+        loop_counter = 0
+        code = "fn main() {\n"
+
+        for token in tokens:
+            if isinstance(token, tkns.While_End):
+                loop_counter -= 1
+                continue
+
+            code += "    " * loop_counter
+
+            if isinstance(token, tkns.Repeatable_Token):
+                code += self.code_dict[type(token)].format(token.times)  # type: ignore
+            else:
+                code += self.code_dict[type(token)]
+
+            if isinstance(token, tkns.While_Start):
+                loop_counter += 1
+
+        return code + "}"
 
 
 def get_generator(language) -> Generator:
     match language:
         case "python":
             return PythonGenerator()
+        case "rust":
+            return RustGenerator()
         case _:
-            raise Exception("Unknown language, accepts: rust, python, c++, c")
+            raise Exception("Unknown language, accepts: rust, python")
